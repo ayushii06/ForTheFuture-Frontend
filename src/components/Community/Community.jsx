db983e2f48276224d9b6cc131f0dca0341a2b3fb
ayushi pal <ayushipal06@gmail.com>
2024-05-24 21:59:55 +0530

    modified UI

--- src/components/Community/Community.jsx
+++ src/components/Community/Community.jsx
@@ -1,34 +1,46 @@
 import React, { useState ,useEffect} from 'react';
 import '../Community/Community.css';
 import Tweet from './Tweet';
+import { useNavigate } from 'react-router-dom';
 
 function Community() {
+  const Navigate=useNavigate();
     const [content, setContent] = useState('');
     const [blog,setBlog]=useState([])
 
+    
+
     const handleInputChange = (event) => {
+      
+    if(!localStorage.getItem('token')){
+      alert('Please register first')
+      Navigate('/register')
+    }
       setContent(event.target.value);
     };
-
-    const user =localStorage.getItem('username')
-
+    
     const handleSubmit = async(e)=>{
+      
+     
         e.preventDefault();
-        const response = await fetch("http://localhost:10000/api/v1/blog/create",{
+        const response = await fetch("http://localhost:10000/api/v1/posts",{
           origin:"http://localhost/10000",
           mode:"cors",
           method:"POST",
           headers:{
               "Content-Type":"application/json",
+              "Authorization":localStorage.getItem('token'),
               "Access-Control-Allow-Origin" : "*"
           },
-          body: JSON.stringify({ body: content, user: user}),
-      });
+          body: JSON.stringify({content: content}),
+      
+              
+    
+            });
             const json = await response.json();
-            console.log(json)
+          
             if (json.success) {
                 alert('Successfully posted')
-            
             }
             else {
                 alert(json.error, "Invalid Credentials")
@@ -39,21 +51,20 @@ function Community() {
 
     const getAllBlogs = async () => {
       //API CALL
-      const response = await fetch("http://localhost:10000/api/v1/blogs/getblog", {
+      const response = await fetch("http://localhost:10000/api/v1/posts", {
         method: "GET",
         headers: {
           "Content-Type": "application/json",
         },
       });
       const json = await response.json()
-    
-      setBlog(json.blogs)
+      console.log(json)
+      setBlog(json)
     }
 
       useEffect(() => {
           getAllBlogs()
       },[])
- 
   return (
 
     <div className="app">
@@ -67,7 +78,7 @@ function Community() {
     </div>
     <main>
       {blog.map(blogs => (
-        <Tweet key={blogs._id} username={blogs.user} body={blogs.body} like={blogs.like} comment={blogs.comment} />
+        <Tweet key={blogs._id} id={blogs._id} username={blogs.user.username} body={blogs.content}  like={blogs.likes} comment={blogs.comments}/>
       ))}
     </main>
   </div>
--- src/components/Community/Tweet.jsx
+++ src/components/Community/Tweet.jsx
@@ -1,10 +1,67 @@
-import React from 'react';
+import React ,{useState} from 'react';
 import '../Community/Community.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faComment, faRetweet, faHeart, faShare } from '@fortawesome/free-solid-svg-icons';
+import { faComment, faHeart } from '@fortawesome/free-solid-svg-icons';
 
 const Tweet = (props) => {
-  const {username,body,like,comment}=props
+  const {id,username,body,like,comment}=props
+  console.log(comment)
+  const [likes,setLikes]=useState()
+  const [comments,setComments]=useState([])
+  const [value,setValue]=useState('')
+
+  function onChange(e){
+    setValue(e.target.value)
+  }
+    const [count,setCount]=useState(0)
+    const [display,setDisplay]=useState(false)
+    const [listdisplay,setListDisplay]=useState(false)
+    async function handleLike(){
+      const response = await fetch(`http://localhost:10000/api/v1/posts/${id}/like`, {
+        origin:"http://localhost/10000",
+          mode:"cors",
+          method:"PUT",
+          headers:{
+              "Content-Type":"application/json",
+              "Authorization":localStorage.getItem('token'),
+              "Access-Control-Allow-Origin" : "*"
+          },
+      });
+      const json = await response.json()
+      
+      setLikes(json.likes.length)
+    
+     }
+     function handleComment(){
+      setDisplay(true)
+     }
+
+
+     async function handleSubmit(){
+      setDisplay(false)
+      setListDisplay(true)
+      const response = await fetch(`http://localhost:10000/api/v1/posts/${id}/comment`, {
+      origin:"http://localhost/10000",
+        mode:"cors",
+        method:"POST",
+        headers:{
+            "Content-Type":"application/json",
+            "Authorization":localStorage.getItem('token'),
+            "Access-Control-Allow-Origin" : "*"
+        },
+        body: JSON.stringify({text: value}),
+      
+    });
+    const json = await response.json()
+    console.log(json)
+    console.log(json.comments)
+    console.log(json.comments.length)
+    setComments(json.comments)
+    setCount(json.comments.length)
+  
+      
+
+     }
   return (
     <div className="tweet-card">
       <div className="tweet-header">
@@ -14,14 +71,39 @@ const Tweet = (props) => {
       </div>
       <p className="tweet-content">{body}</p>
       <div className="tweet-actions">
-        <div className="tweet-action">
-        <FontAwesomeIcon icon={faComment} />  {comment}
+        <div className="tweet-action" onClick={handleComment}>
+        <FontAwesomeIcon icon={faComment} />  {!count=='0'?count:comment.length}
         </div>
-        <div className="tweet-action">
-        <FontAwesomeIcon icon={faHeart} />  {like}
+        <div className="tweet-action" onClick={handleLike}>
+        <FontAwesomeIcon icon={faHeart} />  {!likes=='0'?likes:like.length}
         </div>
-      
+        {!display?<></>: <div style={{display:{display}}} className="toggle">
+      <div className="write-post write-post-toggle">
+      <textarea
+        value={value}
+        onChange={onChange}
+        placeholder="What's on your mind?"
+      ></textarea>
+      <button className='write-post-btn' onClick={handleSubmit}>Post</button>
+    </div>
+      </div>}
+     
       </div>
+      <div  className="comment-section">
+        {comments!=[]?comment.map(c=>{
+          return <>
+          <p className="user">{c.user.username}</p>
+        <p className="comment">{c.text}</p>
+          </>
+        }):comments.map(comment=>{
+          return <>
+          <p className="user">{comment.user.username}</p>
+        <p className="comment">{comment.text}</p>
+          </>
+        })}
+        
+      </div>
+      
     </div>
   );
 }
